a97ad491934f4843741d330e8b46fd1213e3a245
deft21 <sergey2005lesnoy@mail.ru>
2018-07-07 12:16:15 +0300

    UCHi

--- js/javascript.js
+++ js/javascript.js
@@ -1,59 +1,73 @@
-const button = document.querySelector('button');
-const APIkey = '8d214f1a71c91b2ec12a035b0ce91984';
-const Tab = document.getElementById("tab");
+const inputA = document.getElementById("a");//ищу элементы для ввода
+const inputB = document.getElementById("b");//ищу элементы для ввода
 
-Tab.addEventListener("click",function(){
-	var city = document.getElementById('input').value;
-	let url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIkey;
+const a=7,b=4;
+Example(a,b);
 
-	let xhr = new XMLHttpRequest();
-
-	xhr.open('GET', url, false);
-
-	xhr.send();
-
-if (xhr.status!=200){
-			console.log(xhr.status +' ' +xhr.statusText);
-			var text = 'Город не найден';
-			document.write(text);
-} else {
-			let xml = JSON.parse(xhr.responseText);
-			console.log(xml);
-			document.write('</br></br> Ваш город: '+ city);
-			document.write('</br> Температура в К: '+ Math.round(xml.main.temp));
-			document.write('</br> Температура в градусах: ' + Math.round((xml.main.temp - 273)));
-			document.write('</br> Скорость ветра : '+ xml.wind.speed);
-			//document.write(xml.)
-				};
-
-});
-
-
-
-/*
-button.onclick = function(){
-	let city = document.getElementById('input').value;
-	let url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIkey;
-
-	let xhr = new XMLHttpRequest();
-
-	xhr.open('GET', url, false);
-
-	xhr.send();
-
-if (xhr.status!=200){
-			console.log(xhr.status +' ' +xhr.statusText);
-			var text = 'Город не найден';
-			document.write(text);
-} else {
-			let xml = JSON.parse(xhr.responseText);
-			console.log(xml);
-			document.write('</br></br> Ваш город: '+ city);
-			document.write('</br> Температура в К: '+ Math.round(xml.main.temp));
-			document.write('</br> Температура в градусах: ' + Math.round((xml.main.temp - 273)));
-			document.write('</br> Скорость ветра : '+ xml.wind.speed);
-			//document.write(xml.)
-				};
-
+function Example(a,b){
+//вывожу пример
+  const exampleAB = document.getElementById('exampleAB');
+  const result = document.getElementById('result');
+  exampleAB.innerHTML = '<span id = "exampleA">'+a+'</span>+<span id="exampleB">'+b+'</span><span>=</span>';
+  result.innerHTML = '<span class="result">?</span>';
+//рисую первый полукруг
+  var canvasA = document.getElementById('canvasA');
+  var ctxA = canvasA.getContext('2d');
+  
+  ctxA.strokeStyle="red";
+  ctxA.arc(65,150, 60, 0, Math.PI, true);
+  ctxA.stroke();
 };
-*/
+// провера а
+function checkA(){
+  const exampleA = document.getElementById("exampleA");// для смены цвета примера
+  var inputA_v = document.getElementById("a").value;// введенное значение 
+  console.log(inputA_v); 
+  //maxLen=1;
+  //if (inputA.value().length>maxLen) return false; // пытался ограничить по длине??????????????????
+  if(inputA_v!=a){
+  console.log('не равно');
+  console.log (exampleA);
+  exampleA.style.backgroundColor = "yellow";
+  inputA.style.color = "red";
+                } else {
+                  console.log('равно');
+                  exampleA.style.backgroundColor = "white";
+                  inputA.style.color = "black";
+                  inputB.style.display = "block"; // отобразил второе поле для ввода
+                  //рисую второй полукруг
+                  var canvasB = document.getElementById('canvasB');
+                  var ctxB = canvasB.getContext('2d');
+                  
+                  ctxB.strokeStyle="red";
+                  ctxB.arc(65,100, 60, 0, Math.PI, true);
+                  ctxB.stroke();
+                        };
+};
+function checkB(){
+  const exampleB = document.getElementById("exampleB")// для смены цвета примера
+  var inputB_v = document.getElementById("b").value;// введенное значение
+  console.log(inputB);
+  if (inputB_v!=b){
+  console.log('не равно');
+  exampleB.style.backgroundColor = "yellow";
+  inputB.style.color = "red";
+                } else {
+                  console.log('равно');
+                  exampleB.style.backgroundColor = "white";
+                  inputB.style.color = "black";
+                  result.innerHTML = '<input class = "result" type="text" name="result" size="2" id = "answer">';
+                  const answer = document.getElementById('answer');
+                  answer.onkeyup = function checkAnswer(){
+                      var answer_v = document.getElementById('answer').value;
+                      console.log(answer_v);
+                        if (answer_v!=a+b){
+                        console.log('не верно');
+                        answer.style.color = "red";
+                        } else {
+                        console.log('верно');
+                        answer.style.color = "black";
+                        };
+                        }; 
+  };
+};
